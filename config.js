5c525792d9ed1b5c2853da68c85f84168bb5234a
User <zomlesh@yandex.ru>
2021-07-19 17:26:41 +0300

    Работа по бэкенду

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 
-const { PORT = 3000, MONGO_URI = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
+const {
+  PORT = 3000,
+  MONGO_URI = 'mongodb://localhost:27017/bitfilmsdb',
+  JWT_SECRET = 'jwt-secret-token',
+} = process.env;
 
 module.exports = {
   PORT,
   MONGO_URI,
+  JWT_SECRET,
 };
